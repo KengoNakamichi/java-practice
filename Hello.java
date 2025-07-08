6e2a6f86a164831b08ff1e743bbf414bd23dde50
java学習中です <kengo.ytk@icloud.com>
2025-07-08 20:52:19 +0900

    Add Hello.java file

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package Github;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("こんにちは、Github！");
+        System.out.println("初心者ですが、夢の為にめげずに頑張ります！！！");
+    }
+    
+}
