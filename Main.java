9fc3c643731bd541d6f0b7710045b07b371f46ef
java学習中です <kengo.ytk@icloud.com>
2025-07-09 22:02:14 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Github;
+public class Main {
+    public static void main(String[] args) {
+        int number= 10;
+        System.out.println(number);
+        
+        String name="Takeshi";
+        System.out.println(name);
+    }
+    
+}
